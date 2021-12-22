037936caae0eb8ca5634f58ee144796957e8b6cb
Laurent Sauzet <59488676+LaurentSauzetOff@users.noreply.github.com>
2021-12-22 18:53:04 +0100

    Initializing the apploication

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,14 @@
 
 <script>
     export default {
-        
+        data: function(){
+            return {
+                quotes: [
+                    'Lire, c\'est refuser de mourir.'
+                ],
+                maxQuotes: 10
+            }
+        }
     }
 </script>
 
